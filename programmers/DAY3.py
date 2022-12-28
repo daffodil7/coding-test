096e8116e8fe3ee7d4b1ca7a5b5529df3c975f11
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2022-12-28 19:01:48 +0900

    Create DAY3.py

---
+++ programmers/DAY3.py
@@ -0,0 +1,10 @@
+# 다시 풀 문제
+
+# 문자 반복 출력하기
+# 문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
+def solution(my_string, n):
+    answer = []
+    for i in range(len(my_string)):
+        for j in range(n):
+            answer.append(my_string[i])
+    return ''.join(answer)
