4bfa01e11a52a47a3e3aaf2b623290265a20197b
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2023-01-03 22:15:10 +0900

    Update DAY4.py

--- programmers/DAY4.py
+++ programmers/DAY4.py
@@ -1,10 +1,18 @@
 # 다시 풀 문제
-# 양꼬치 문제
-# 머쓱이네 양꼬치 가게는 10인분을 먹으면 음료수 하나를 서비스 양꼬치는 1인분에 12,000원, 음료수는 2,000원 
-# 정수 n과 k가 매개변수로 주어졌을 때, 양꼬치 n인분과 음료수 k개를 먹었다면 총얼마를 지불해야 하는지 return 하도록 solution 함수를 완성
-
 
 # 배열 자르기
 # 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성
 def solution(numbers, num1, num2):
     return numbers[num1:num2+1]
+
+# 외계행성 나이
+# 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성
+def solution(age):
+    answer = ''
+    return answer.join([chr(int(i)+97) for i in str(age)])
+
+# 방법2
+def solution(age):
+    conv = {'0':'a','1':'b','2':'c','3':'d','4':'e'
+            ,'5':'f','6':'g','7':'h','8':'i','9':'j'}
+    return ''.join(conv[i] for i in str(age))
