9aaf00ba012d8dadde4d1abe3fd115b4f96e5c82
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2023-01-05 20:46:57 +0900

    Update DAY5.py

--- programmers/DAY5.py
+++ programmers/DAY5.py
@@ -12,7 +12,18 @@ import math
 def solution(balls, share):
     return math.comb(balls, share)
   
-  
 # 공 던지기
 def solution(numbers, k):
     return numbers[2 * (k - 1) % len(numbers)]
+
+
+# 배열 회전시키기
+from collections import deque
+
+def solution(numbers, direction):
+    num_deque = deque(numbers)
+    if direction == 'right':
+        num_deque.rotate(1)
+    else:
+        num_deque.rotate(-1)
+    return list(num_deque)
