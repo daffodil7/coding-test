5bbf963754ee951c9bf1c7c4d447c8e1879693ac
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2023-01-05 20:39:37 +0900

    Create DAY5.py

---
+++ programmers/DAY5.py
@@ -0,0 +1,18 @@
+# 구슬을 나누는 경우의 수
+# 나만의 풀이
+import math
+def solution(balls, share):
+    how = balls - share
+    b = (math.factorial(how) * math.factorial(share))
+    answer = math.factorial(balls) / b
+    return answer
+  
+# 다른 사람 풀이
+import math
+def solution(balls, share):
+    return math.comb(balls, share)
+  
+  
+# 공 던지기
+def solution(numbers, k):
+    return numbers[2 * (k - 1) % len(numbers)]
