7e98d04cd31db6095df7677ef924ce7492ed5d88
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2023-01-09 22:50:57 +0900

    Update DAY6.py

--- programmers/DAY6.py
+++ programmers/DAY6.py
@@ -28,3 +28,12 @@ def solution(n):
                 output += 1
                 break
     return output
+
+# 팩토리얼
+from math import factorial
+
+def solution(n):
+    k = 10
+    while n < factorial(k):
+        k -= 1
+    return k
