570be21a7931e087b748e09dcd5b91b4d3196369
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2023-01-17 20:06:59 +0900

    Create DAY9.py

---
+++ programmers/DAY9.py
@@ -0,0 +1,17 @@
+# 다시 풀 문제
+# 영어가 싫어요
+# 내 풀이
+def solution(numbers):
+    nums = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    for index,nums in enumerate(nums):
+        numbers = numbers.replace(nums, str(index))
+    return int(numbers)
+
+# 다른 사람 풀이
+def solution(numbers):
+    dic = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    i=0
+    for word in dic:
+        numbers = numbers.replace(word, str(i))
+        i+=1
+    return int(numbers)
