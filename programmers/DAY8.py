42276c5b2860f5158ea88253aecfd673d3193144
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2023-01-17 17:22:10 +0900

    Create DAY8.py

---
+++ programmers/DAY8.py
@@ -0,0 +1,21 @@
+# 다시 풀 문제
+# 가까운 수
+# 내 풀이
+def solution(array, n):
+    array.sort()
+    answer = 0
+    com = n+100
+    for i in array:
+        if abs(i-n) < com:
+            com = abs(i-n)
+            answer = i
+    return answer
+
+# 다른 사람 풀이
+def solution(array, n):
+    array.sort()
+    temp = []
+    for i in array :
+        temp.append( abs(n-i) )
+
+    return array[temp.index(min(temp))]
