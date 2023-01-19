84626e3a6d402ef4cf7cd0145fb72d07d4d6c5fe
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2023-01-19 23:03:39 +0900

    Update DAY11.py

--- programmers/DAY11.py
+++ programmers/DAY11.py
@@ -5,3 +5,28 @@ def solution(n):
     for n in str(n):
         answer += int(n)
     return answer
+
+# OX 퀴즈
+def solution(quiz):
+    answer = []
+    for i in quiz:
+        s = i.split()
+        if s[1] == '-':
+            if (int(s[0]) - int(s[2])) == int(s[len(s)-1]):
+                answer.append('O')
+            else:
+                answer.append('X')
+        if s[1] == '+':
+            if (int(s[0]) + int(s[2])) == int(s[len(s)-1]):
+                answer.append('O')
+            else:
+                answer.append('X')
+    return answer
+
+# 다른 풀이
+def valid(equation):
+    equation = equation.replace('=', '==')
+    return eval(equation)
+
+def solution(equations):
+    return ["O" if valid(equation) else "X" for equation in equations]
