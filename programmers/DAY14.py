5f0ec0c533f2e3a3abe48736da4d6f8148a039aa
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2023-01-26 20:11:12 +0900

    Update DAY14.py

--- programmers/DAY14.py
+++ programmers/DAY14.py
@@ -28,3 +28,25 @@ def solution(keyinput, board):
         elif i == "down" and now[1]-1 >= -stan1:
             now[1] -= 1
     return(now)
+
+# 다항식 더하기
+def solution(polynomial):
+    x, num = 0, 0
+    polynomial = polynomial.split(' + ')
+    for i in polynomial:
+        if 'x' not in i:
+            num += int(i)
+        else:
+            if len(i) == 1:
+                x += 1
+            else:
+                x += int(i[:-1])
+    if x == 0 and num == 0:
+        return
+    if x == 0:
+        return str(num)
+    if x == 1:
+        x = ""
+    if num == 0:
+        return str(x) + 'x'
+    return str(x)+'x + '+str(num)
