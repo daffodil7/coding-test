2be70e1e927bf5dc491d0166170ffe6c463604b6
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2023-02-05 23:42:05 +0900

    Update DAY17.py

--- programmers/DAY17.py
+++ programmers/DAY17.py
@@ -8,3 +8,23 @@ def solution(n):
             answer += 1
     return answer
   
+# 평행
+from itertools import combinations
+
+def solution(dots):
+    a = []
+    for (x1,y1),(x2,y2) in combinations(dots,2):
+        a.append((y2-y1,x2-x1))
+
+    for (x1,y1),(x2,y2) in combinations(a,2):
+        if x1*y2==x2*y1:
+            return 1
+    return 0
+
+# 다른 풀이
+def solution(dots):
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]=dots
+    answer1 = ((y1-y2)*(x3-x4) == (y3-y4)*(x1-x2))
+    answer2 = ((y1-y3)*(x2-x4) == (y2-y4)*(x1-x3))
+    answer3 = ((y1-y4)*(x2-x3) == (y2-y3)*(x1-x4))
+    return 1 if answer1 or answer2 or answer3 else 0
