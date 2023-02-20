cbb9efc7e0946174a765f15026ef86268dd83e3c
Ja-Young <87185119+daffodil7@users.noreply.github.com>
2023-02-20 22:44:16 +0900

    Update DAY18.py

--- programmers/DAY18.py
+++ programmers/DAY18.py
@@ -8,7 +8,15 @@ def solution(a, b):
     while b%5==0:
         b//=5
     return 1 if b==1 else 2
-  
+
+# math.gcd : 최대공약수 구하는 함수임
+
 # 다른 풀이
 def solution(a, b):
     return 1 if a/b * 1000 % 1 == 0 else 2
+
+# 겹치는 선분의 길이
+# &는 집합의 교집합을, |는 집합의 합집합을 추출
+def solution(lines):
+    sets = [set(range(min(i), max(i))) for i in lines]
+    return len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])
